4cff5a61c2ca7fa9f474fc3c7ba8f2454e639d6f
Garrett Wells <garrettdwells@gmail.com>
2014-11-25 20:35:47 -0800

    Ran create corpus to generate two data sets

--- create_corpus.py
+++ create_corpus.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-def scrapeData(url):
+def scrape_data(url):
 	res = requests.get(url)
 	soup = BeautifulSoup(res.text)
 
@@ -11,23 +11,31 @@ def scrapeData(url):
 
 	return "".join(article_text)
 
+
 # read in articles_url.txt
-data = open("article_urls.txt", "r")
-articles = data.readlines()
+with open("article_urls.txt", "r") as infile, open("subjective.data", "w") as subjective, open("objective.data", "w") as objective:
 
-# the two corpuses that will be created
-subjective = open("subjective.data", "w")
-objective = open("objective.data", "w")
-
-for article in articles:
-	content = article.split("\t")
-	article_text = scrapeData(content[1].strip())
-
-	if content[0] == "OpEd":
-		subjective.write("%s%s" % (article_text.encode("utf8"), "\n\n"))
-		subjective.write("==============================\n\n")
-	else:
-		objective.write("%s%s" % (article_text.encode("utf8"), "\n\n"))
-		objective.write("==============================\n\n")
+	for line in infile:
+		type, url = line.split("\t")
+		article_text = scrape_data(url.strip())
+		print 'i'
+		if type == "OpEd":
+			subjective.write("%s\n\n" % article_text.encode("utf8"))
+		else:
+			objective.write("%s\n\n" % article_text.encode("utf8"))
 
 
+# # the two corpuses that will be created
+# subjective = open("subjective.data", "w")
+# objective = open("objective.data", "w")
+
+# for article in articles:
+# 	content = article.split("\t")
+# 	article_text = scrape_data(content[1].strip())
+
+# 	if content[0] == "OpEd":
+# 		subjective.write("%s%s" % (article_text.encode("utf8"), "\n\n"))
+# 		subjective.write("==============================\n\n")
+# 	else:
+# 		objective.write("%s%s" % (article_text.encode("utf8"), "\n\n"))
+# 		objective.write("==============================\n\n")
