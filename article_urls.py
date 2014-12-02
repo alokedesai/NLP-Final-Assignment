be438ed9311e0efcc6603cfc8b9dd004edf9dd1e
Aloke Desai <aloke.desai@gmail.com>
2014-12-02 09:29:49 -0800

    create separate query for just NYT OpEd data

--- article_urls.py
+++ article_urls.py
@@ -1,9 +1,27 @@
 import requests
 
-base_url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=obamacare&fl=web_url%2Cnews_desk&api-key=17a92e5aa402d751f17bff6ed5e9f0f1:19:49094860&page="
+# code for objective articles
 
-for i in xrange(1,101):
+# base_url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=obamacare&fl=web_url%2Cnews_desk&api-key=17a92e5aa402d751f17bff6ed5e9f0f1:19:49094860&page="
+
+# for i in xrange(1,101):
+# 	r = requests.get("%s%d" % (base_url, i))
+# 	data = r.json()
+# 	for article in data["response"]["docs"]:
+# 		print "%s\t%s" % (article["news_desk"],article["web_url"])
+
+base_url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=affordable+care+act&fq=news_desk%3A%28%22OpEd%22%29&fl=web_url%2Cnews_desk&api-key=17a92e5aa402d751f17bff6ed5e9f0f1:19:49094860&page="
+for i in xrange(1,20):
 	r = requests.get("%s%d" % (base_url, i))
 	data = r.json()
 	for article in data["response"]["docs"]:
 		print "%s\t%s" % (article["news_desk"],article["web_url"])
+
+
+base_url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=obamacare&fq=news_desk%3A%28%22OpEd%22%29&fl=web_url%2Cnews_desk&api-key=17a92e5aa402d751f17bff6ed5e9f0f1:19:49094860&page="
+for i in xrange(1,20):
+	r = requests.get("%s%d" % (base_url, i))
+	data = r.json()
+	for article in data["response"]["docs"]:
+		print "%s\t%s" % (article["news_desk"],article["web_url"])
+
